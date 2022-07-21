42426073b25779df1a18c624d6a658e213c26317
Elevattd <marianoo.14.md@gmail.com>
2022-07-21 20:37:43 -0300

    feat(client): new lines

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -1,93 +1,100 @@
-import React, { useRef } from 'react';
-import './Work.scss';
-import { motion } from 'framer-motion';
-import { AppWrap, MotionWrap } from '../../wrapper';
-import { images } from '../../constants';
-import { SiVercel } from 'react-icons/si';
-import { ImGithub } from 'react-icons/im';
+import React, { useRef } from "react";
+import "./Work.scss";
+import { motion } from "framer-motion";
+import { AppWrap, MotionWrap } from "../../wrapper";
+import { images } from "../../constants";
+import { SiVercel } from "react-icons/si";
+import { ImGithub } from "react-icons/im";
 
 const Work = () => {
-	const carousel = useRef();
-	const projects = [
-		{
-			title: 'Pokemon App',
-			url: 'https://app-pokedex-pi.vercel.app/',
-			imgUrl: images.pokemon,
-			urlR: 'https://github.com/Elevattd/Pokedex',
-			description:
-				'Node.js, Express, Sequelize, PostgreSQL, HTML, CSS, Javascript, React, Redux, entre otras..',
-		},
-		{
-			title: 'yoVoy!',
-			url: 'https://yovoy-frontend.herokuapp.com/',
-			imgUrl: images.yovoy,
-			urlR: 'https://github.com/Elevattd/yoVoy-',
-			description:
-				'Typescript, HTML, CSS, React, Redux, Json Web Token, Material UI.',
-		},
-		{
-			title: 'youEconomy',
-			url: 'http://',
-			imgUrl: images.youEconomy,
-			urlR: 'https://github.com/Elevattd/youEconomy',
-			description: 'Futura description',
-		},
-		{
-			title: 'Netflix Clone',
-			url: 'http://',
-			imgUrl: images.netflix,
-			description: 'Futura description',
-		},
-	];
+  const carousel = useRef();
+  const projects = [
+    {
+      title: "Pokemon App",
+      url: "https://app-pokedex-pi.vercel.app/",
+      imgUrl: images.pokemon,
+      urlR: "https://github.com/Elevattd/Pokedex",
+      description:
+        "Node.js, Express, Sequelize, PostgreSQL, HTML, CSS, Javascript, React, Redux, entre otras..",
+    },
+    {
+      title: "yoVoy!",
+      url: "https://yovoy-frontend.herokuapp.com/",
+      imgUrl: images.yovoy,
+      urlR: "https://github.com/Elevattd/yoVoy-",
+      description:
+        "Typescript, HTML, CSS, React, Redux, Json Web Token, Material UI.",
+    },
+    {
+      title: "youEconomy",
+      url: "",
+      imgUrl: images.youEconomy,
+      urlR: "https://github.com/Elevattd/youEconomy",
+      description:
+        "React Js, Node Js, Redux Toolkit, Express, Sequelize, Jsonwebtoken, Boostrap. ",
+    },
+    {
+      title: "Netflix Clone",
+      url: "",
+      imgUrl: images.netflix,
+      urlR: "",
+      description: "",
+    },
+  ];
 
-	return (
-		<div>
-			<h2 className="head-text">
-				Proyectos <span>realizados</span>
-			</h2>
-			<motion.div ref={carousel} className="slider-container">
-				<motion.div
-					drag="x"
-					dragConstraints={{ left: -350, right: 0 }}
-					className="slider"
-				>
-					{projects.map((project, index) => (
-						<motion.div
-							whileInView={{ opacity: 1 }}
-							whileHover={{ scale: 1.1 }}
-							transition={{ duration: 0.5, type: 'tween' }}
-							className="app__work-item"
-							key={project.title + index}
-						>
-							<div className="Card_bg">
-								<img
-									src={project.imgUrl}
-									draggable="false"
-									alt={project.title}
-								/>
-								<div className="Card_bg_li">
-									<a href={project.urlR} target="_blank">
-										<ImGithub size={20} />
-										<p>Repositorio</p>
-									</a>
-									<a href={project.url} target="_blank">
-										<SiVercel size={20} />
-										<p>Deploy</p>
-									</a>
-								</div>
-							</div>
-							<h2 className="bold-text" style={{ marginTop: 20 }}>
-								{project.title}
-							</h2>
-							<p className="p-text" style={{ marginTop: 10 }}>
-								{project.description}
-							</p>
-						</motion.div>
-					))}
-				</motion.div>
-			</motion.div>
-		</div>
-	);
+  return (
+    <div>
+      <h2 className="head-text">
+        Proyectos <span>realizados</span>
+      </h2>
+      <motion.div ref={carousel} className="slider-container">
+        <motion.div
+          drag="x"
+          dragConstraints={{ left: -350, right: 0 }}
+          className="slider"
+        >
+          {projects.map((project, index) => (
+            <motion.div
+              whileInView={{ opacity: 1 }}
+              whileHover={{ scale: 1.1 }}
+              transition={{ duration: 0.5, type: "tween" }}
+              className="app__work-item"
+              key={project.title + index}
+            >
+              <div className="Card_bg">
+                <img
+                  src={project.imgUrl}
+                  draggable="false"
+                  alt={project.title}
+                />
+                <div className="Card_bg_li">
+                  {project.urlR !== "" ? (
+                    <a href={project.urlR} target="_blank">
+                      <ImGithub size={20} />
+                      <p>Repositorio</p>
+                    </a>
+                  ) : null}
+
+                  {project.url !== "" ? (
+                    <a href={project.url} target="_blank">
+                      <ImGithub size={20} />
+                      <p>Deploy</p>
+                    </a>
+                  ) : null}
+                </div>
+              </div>
+              <h2 className="bold-text" style={{ marginTop: 20 }}>
+                {project.title}
+              </h2>
+              <p className="p-text" style={{ marginTop: 10 }}>
+                {project.description}
+              </p>
+            </motion.div>
+          ))}
+        </motion.div>
+      </motion.div>
+    </div>
+  );
 };
 
-export default AppWrap(MotionWrap(Work, 'app__work'), 'work', 'app__whitebg');
+export default AppWrap(MotionWrap(Work, "app__work"), "work", "app__whitebg");
